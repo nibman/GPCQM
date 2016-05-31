4466349f21bdbabba31e514a49aa89ac97f87d2d
eric poirier <gronour@erics-MacBook-Pro.local>
2016-05-31 17:54:35 -0400

    s

--- public/assets/js/AthletesClient.js
+++ public/assets/js/AthletesClient.js
@@ -3,7 +3,7 @@ Parse.serverURL = 'http://gpcqm.herokuapp.com/parse'
 
 Athletes = 
  {
-    keys:["firstName", "lastName", "athleteID", "dossard", "hrm", "srmPowerMeter", 
+    keys:["objectId", "firstName", "lastName", "athleteID", "dossard", "hrm", "srmPowerMeter", 
             "vectorPowerMeter", "quarqPowerMeter", "speedCadence", "powerMeter", 
             "garminSpeedCadence", "team", "gps", "wheelCir"],
          
@@ -39,7 +39,7 @@ Athletes =
                     for (var j=0; j<k.length; ++j)
                     {
                         e[k[j]] = object[i].get(k[j]);
-                    }
+                    };
                     a.push(e); 
                 }
                 successCB(a);
@@ -50,13 +50,96 @@ Athletes =
             }
          });
     },
-    setAthlete:function(athleteID, athleteObj, successCB, error)
+    saveAthlete:function(athleteID, athleteObj, successCB, e)
     {
+        if (athleteID != null)
+        {
+            var Athlete = Parse.Object.extend("Athlete");
+            var query = new Parse.Query(Athlete);
+            query.get(id, 
+            { 
+                success:function(athlete)
+                {
+                    for(var e in athleteObj)
+                    {
+                        if (e != "objectId")
+                        {
+                            athlete.set(e, athleteObj[e]);
+                        }
+                    }
+                    athlete.save(null, {
+                        success:function(athlete)
+                        {
+                            var k = Athletes.keys;
+                            var e =  { };
+                            for (var j=0; j<k.length; ++j)
+                            {
+                                e[k[j]] = athlete.get(k[j]);
+                            };
+                            successCB(e);
+                        },
+                        error:function(object, error)
+                        {
+                            e != null ? e(error) : errorDefault(error);
+                        }
+                    })
+                },
+                error:function(object, error)
+                {
+                    e != null ? e(error) : errorDefault(error);
+                }
+            });
+        }
+        else
+        {
+            Athlete.createAthlete(athleteObj,successCB, e);
+        }
+    },
+    createAthlete:function(athleteObj, successCB, error)
+    {
+        var Athlete = Parse.Object.extend("Athlete");
+        var a = new Athlete();
+        for (var e in athleteObj)
+        {
+            a.set(e, athleteObj[e]);
+        }
         
+        a.save(null, {
+            success: function(a)
+            {
+                successCB(a);
+            },
+            error:function(a, e)
+            {
+                e != null ? e(error) : errorDefault(error);
+            } 
+        });
     },
     deleteAthlete:function(athleteID)
     {
-        
+        var Athlete = Parse.Object.extend("Athlete");
+            var query = new Parse.Query(Athlete);
+            query.get(id, 
+            { 
+                success:function(athlete)
+                {
+                    
+                    athlete.destroy({
+                        success:function(athlete)
+                        {
+                            successCB();
+                        },
+                        error:function(object, error)
+                        {
+                            e != null ? e(error) : errorDefault(error);
+                        }
+                    });
+                },
+                error:function(object, error)
+                {
+                    e != null ? e(error) : errorDefault(error);
+                }
+            });
     },
     errorDefault:function(e)
     {
