60e04d331f6b7774f77c4211f97f4b4084705475
eric poirier <gronour@erics-MacBook-Pro.local>
2016-07-12 22:26:57 -0400

    again

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
-
+app.use('/public/maps', express.static(path.join(__dirname, '/public/maps')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
